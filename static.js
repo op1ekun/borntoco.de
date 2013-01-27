f293a36a74b4bb9767d585862888725ed314ae66
op1ekun <woociek@gmail.com>
2013-01-27 23:48:55 +0100

    static routing + html pages

--- static.js
+++ static.js
@@ -1,4 +1,22 @@
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(8888);
+var express = require('express'),
+        app = express();
+
+    app.get('/about', function(req, res) {
+        res.sendfile(__dirname + '/about.html');
+    });
+    
+    app.get('/blog', function(req, res) {
+        res.sendfile(__dirname + '/blog.html');
+    });
+    
+    app.get('/works', function(req, res) {
+        res.sendfile(__dirname + '/works.html');
+    });
+    
+    app.get('/contact', function(req, res) {
+        res.sendfile(__dirname + '/contact.html');
+    });
+    
+    app.use(express.static(__dirname));
+
+    app.listen(8888);
